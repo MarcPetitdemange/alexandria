bc663aeb5fadb493e033f4f75585ebcbd564b0a0
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-10-10 23:18:04 +0200

    Ajout des premières pages de connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: 'login',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'create-account',
+    loadChildren: () => import('./create-account/create-account.module').then( m => m.CreateAccountPageModule)
+  },
+  {
+		path: 'tabs',
+		loadChildren: () => import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+    canLoad: [AuthGuard]
+	},
+  {
+		path: '',
+		redirectTo: '/login',
+		pathMatch: 'full'
+	}
 ];
 @NgModule({
   imports: [
